5fedb2af93da4e9734a581eabf9c9d1dd86e792a
raphaelmstudios <r.musau@alustudent.com>
2026-02-23 23:41:03 +0200

    gather-data

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -8,17 +8,20 @@ if __name__ == "__main__":
     employee_id = int(sys.argv[1])
     base_url = "https://jsonplaceholder.typicode.com"
 
-    user = requests.get("{}/users/{}".format(base_url, employee_id)).json()
-    todos = requests.get("{}/todos?userId={}".format(
-        base_url, employee_id)).json()
+    user = requests.get(
+        "{}/users/{}".format(base_url, employee_id)
+    ).json()
+    todos = requests.get(
+        "{}/todos".format(base_url), params={"userId": employee_id}
+    ).json()
 
     employee_name = user.get("name")
-    done_tasks = [task for task in todos if task.get("completed")]
     total_tasks = len(todos)
+    done_tasks = [task for task in todos if task.get("completed") is True]
     number_of_done_tasks = len(done_tasks)
 
     print("Employee {} is done with tasks({}/{}):".format(
         employee_name, number_of_done_tasks, total_tasks))
 
     for task in done_tasks:
-        print("\t {}".format(task.get("title"))i)
+        print("\t {}".format(task.get("title")))
